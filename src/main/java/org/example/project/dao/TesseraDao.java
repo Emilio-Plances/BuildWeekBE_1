b4a203cbf39bb347cd8d17d624e24e3c834e4eb1
Tommaso Cantarini <tommasocantarini4@gmail.com>
2024-01-29 15:44:55 +0100

    feat:tesseraDao

--- src/main/java/org/example/project/dao/TesseraDao.java
+++ src/main/java/org/example/project/dao/TesseraDao.java
@@ -1,6 +1,8 @@
 package org.example.project.dao;
 
 import jakarta.persistence.*;
+import org.example.project.entities.TesseraCliente;
+import org.example.project.entities.Tratta;
 
 import java.util.List;
 
@@ -12,21 +14,34 @@ public class TesseraDao {
         this.emf = Persistence.createEntityManagerFactory("biglietteria");
         this.em = emf.createEntityManager();
     }
-    public void save (TesseraDao a){
-        EntityTransaction et = em.getTransaction();
-        et.begin();
-        em.persist(a);
-        et.commit();
+    public void save (Tratta a){
+        try {
+            EntityTransaction et = em.getTransaction();
+            et.begin();
+            em.persist(a);
+            et.commit();
+        }catch (Exception e ){
+            e.printStackTrace();
+        }
     }
 
-    public TesseraDao getById(int id) {
-        return em.find(TesseraDao.class,id);
+    public Tratta getById(int id) {
+        return em.find(Tratta.class,id);
     }
-    public void delete (TesseraDao a){
-        EntityTransaction et = em.getTransaction();
-        et.begin();
-        TesseraDao art = getById(a.getId());
-        em.remove(art);
-        et.commit();
+    public void delete (Tratta a){
+        try{
+            EntityTransaction et = em.getTransaction();
+            et.begin();
+            Tratta art = getById(a.getId());
+            em.remove(art);
+            et.commit();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void close() {
+        emf.close();
+        em.close();
     }
 }
