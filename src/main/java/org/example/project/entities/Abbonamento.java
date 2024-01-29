a9d4799bc572fd953f1337fa5eecabb4c8df7db3
calogeroteresi <caloteresi@gmail.com>
2024-01-29 15:26:18 +0100

    ProdottoAcquistato

--- src/main/java/org/example/project/entities/Abbonamento.java
+++ src/main/java/org/example/project/entities/Abbonamento.java
@@ -1,9 +1,61 @@
 package org.example.project.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import org.example.project.enums.TipoAbbonamento;
+
+import java.time.LocalDate;
 
 @Entity
 @Table(name="abbonamenti")
 public class Abbonamento extends ProdottoAcquistato{
+
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "tipo_abbonamento")
+    private TipoAbbonamento tipoAbbonamento;
+
+    @Column(name = "fine_validita")
+    private LocalDate fineValidita;
+
+    @ManyToOne
+    @JoinColumn (name="tessera_cliente_id")
+    private TesseraCliente tesseraCliente;
+
+    public Abbonamento() {
+    }
+
+    public TipoAbbonamento getTipoAbbonamento() {
+        return tipoAbbonamento;
+    }
+
+    public void setTipoAbbonamento(TipoAbbonamento tipoAbbonamento) {
+        this.tipoAbbonamento = tipoAbbonamento;
+    }
+
+    public LocalDate getFineValidita() {
+        return fineValidita;
+    }
+
+    public void setFineValidita(LocalDate fineValidita) {
+        this.fineValidita = fineValidita;
+    }
+
+    public TesseraCliente getTesseraCliente() {
+        return tesseraCliente;
+    }
+
+    public void setTesseraCliente(TesseraCliente tesseraCliente) {
+        this.tesseraCliente = tesseraCliente;
+    }
+
+    @Override
+    public String toString() {
+        return "Abbonamento{" +
+                "tratta=" + getTratta() +
+                "dataAcquisto=" + getDataAcquisto() +
+                "tipoAbbonamento=" + tipoAbbonamento +
+                ", fineValidita=" + fineValidita +
+                ", tesseraCliente=" + tesseraCliente +
+                '}';
+    }
 }
--- src/main/java/org/example/project/entities/ProdottoAcquistato.java
+++ src/main/java/org/example/project/entities/ProdottoAcquistato.java
@@ -1,11 +1,69 @@
 package org.example.project.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.InheritanceType;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
 
 @Entity
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
-public abstract class ProdottoAcquistato {
+public class ProdottoAcquistato {
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "sequenza_libreria")
+    @SequenceGenerator(name="sequenza_libreria",initialValue = 1,allocationSize = 1)
+    private int id;
+
+    @Column(name="data_acquisto")
+    private LocalDate dataAcquisto;
+
+    @ManyToOne
+    @JoinColumn(name="venditore_id")
+    private Venditore venditore;
+
+    @ManyToOne
+    @JoinColumn(name="tratta_id")
+    private Tratta tratta;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDate getDataAcquisto() {
+        return dataAcquisto;
+    }
+
+    public void setDataAcquisto(LocalDate dataAcquisto) {
+        this.dataAcquisto = dataAcquisto;
+    }
+
+    public Venditore getVenditore() {
+        return venditore;
+    }
+
+    public void setVenditore(Venditore venditore) {
+        this.venditore = venditore;
+    }
+
+    public Tratta getTratta() {
+        return tratta;
+    }
+
+    public void setTratta(Tratta tratta) {
+        this.tratta = tratta;
+    }
+
+    @Override
+    public String toString() {
+        return "ProdottoAcquistato{" +
+                "id=" + id +
+                ", dataAcquisto=" + dataAcquisto +
+                ", venditore=" + venditore +
+                ", tratta=" + tratta +
+                '}';
+    }
 }
