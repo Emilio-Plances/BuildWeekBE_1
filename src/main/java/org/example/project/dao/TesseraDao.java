355ce1d3f44d1827a07056c1c634552a21fdebd3
calogeroteresi <caloteresi@gmail.com>
2024-01-30 14:12:09 +0100

    Commit

--- src/main/java/org/example/project/dao/TesseraDao.java
+++ src/main/java/org/example/project/dao/TesseraDao.java
@@ -2,9 +2,10 @@ package org.example.project.dao;
 
 import jakarta.persistence.*;
 import org.example.project.entities.TesseraCliente;
-import org.example.project.entities.Tratta;
 
+import java.time.LocalDate;
 import java.util.List;
+
 
 public class TesseraDao {
     private EntityManagerFactory emf;
@@ -14,25 +15,25 @@ public class TesseraDao {
         this.emf = Persistence.createEntityManagerFactory("biglietteria");
         this.em = emf.createEntityManager();
     }
-    public void save (Tratta a){
+    public void save (TesseraCliente t){
         try {
             EntityTransaction et = em.getTransaction();
             et.begin();
-            em.persist(a);
+            em.persist(t);
             et.commit();
         }catch (Exception e ){
             e.printStackTrace();
         }
     }
 
-    public Tratta getById(int id) {
-        return em.find(Tratta.class,id);
+    public TesseraCliente getById(int id) {
+        return em.find(TesseraCliente.class,id);
     }
-    public void delete (Tratta a){
+    public void delete (TesseraCliente a){
         try{
             EntityTransaction et = em.getTransaction();
             et.begin();
-            Tratta art = getById(a.getId());
+            TesseraCliente art = getById(a.getTessera_cliente());
             em.remove(art);
             et.commit();
         }catch (Exception e){
@@ -44,6 +45,77 @@ public class TesseraDao {
         emf.close();
         em.close();
     }
+
+
+    public boolean controllaTesseraById(int id){
+        TesseraCliente t = getById(id);
+        return t.isStatoTessera();
+    }
+
+    public void disattivaTessera(int id){
+        TesseraCliente t = getById(id);
+        boolean statoTessera = controllaTesseraById(id);
+
+        try{
+            if (statoTessera) {
+                t.setStatoTessera(false);
+                EntityTransaction et = em.getTransaction();
+                et.begin();
+                em.merge(t);
+                et.commit();
+        }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void attivaTessera(int id){
+        TesseraCliente t = getById(id);
+        boolean statoTessera = controllaTesseraById(id);
+
+        try{
+            if (statoTessera) {
+                t.setStatoTessera(false);
+                EntityTransaction et = em.getTransaction();
+                et.begin();
+                em.merge(t);
+                et.commit();
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void controllaOgniTessera(){
+        try {
+            LocalDate currentDate = LocalDate.now();
+            Query q = em.createNamedQuery("getListaTessereScadute");
+            q.setParameter("currentDate", currentDate);
+
+            List<TesseraCliente> tessereScadute = q.getResultList();
+
+            EntityTransaction et = em.getTransaction();
+            et.begin();
+
+            for (TesseraCliente tessera : tessereScadute) {
+                tessera.setStatoTessera(false);
+                em.merge(tessera);
+            }
+
+            et.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
+
+
 
 
 }
